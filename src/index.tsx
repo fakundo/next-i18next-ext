0c32a485d7cd7ec4f737df25d2124a2008db8140
fakundo <fakundo@users.noreply.github.com>
2026-02-10 01:41:12 +0500

    fix: fix global translations

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,15 @@ import { useMemo } from 'react';
 
 let GLOBAL_IN_APP: any;
 
-const getFromDoc = () => {
-  try {
-    GLOBAL_IN_APP = GLOBAL_IN_APP || JSON.parse(document.getElementById(SCRIPT_ID)!.textContent!);
-  } catch {}
-  return GLOBAL_IN_APP;
-};
+try {
+  GLOBAL_IN_APP = JSON.parse(document.getElementById(SCRIPT_ID)!.textContent!);
+} catch {}
 
 export const appWithTranslation = (App: any, configOverride?: UserConfig | null) => {
   const AppWithTrans = originalAppWithTranslation(App, configOverride);
 
   const WrappedApp = (appProps: any) => {
-    const { [PROP_NAME]: inApp = getFromDoc(), pageProps, ...rest } = appProps;
+    const { [PROP_NAME]: inApp = GLOBAL_IN_APP, pageProps, ...rest } = appProps;
     const { [PROP_NAME]: inPage } = pageProps;
     const i18n = useMemo(() => mergeTranslations(inApp, inPage), [inApp, inPage]);
     return <AppWithTrans {...rest} pageProps={{ ...pageProps, [PROP_NAME]: i18n }} />;
